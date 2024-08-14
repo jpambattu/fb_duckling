400958ed7a6178bd76e46693f3f14eff6a4a4fb6
Joseph Pious <44723605+jpambattu@users.noreply.github.com>
2024-08-14 11:45:37 +0530

    fix in reftime base_class.py

--- src/fb_duckling/base_class.py
+++ src/fb_duckling/base_class.py
@@ -7,12 +7,11 @@ class BaseClass(object):
     default_url = get_default_url()  # http://0.0.0.0
     default_port = get_default_port()  # 8000
     default_timezone = get_default_timezone() # America/Los_Angeles
-    default_reftime = get_default_reftime()   # Unix epoch in milliseconds
 
-    def __init__(self, locale=default_locale, url=default_url, port=default_port, timezone=default_timezone, reftime=default_reftime):
+    def __init__(self, locale=default_locale, url=default_url, port=default_port, timezone=default_timezone, reftime=None):
 
         self.locale = locale or "en_US"
         self.url = url or "http://0.0.0.0"
         self.port = port or 8000
         self.timezone = timezone or "America/Los_Angeles"
-        self.reftime = reftime
+        self.reftime = reftime if reftime else get_default_reftime()   # Unix epoch in milliseconds
